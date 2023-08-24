9f267adc945a70721268d1d99bfff7a734dcbb37
GlebHal <hammanov.gleb@gmail.com>
2023-08-24 14:00:20 +0300

    Made start window logic

---
+++ DOTNET-WPF-Shop/Modules/Start/Start.provider.cs
@@ -0,0 +1,26 @@
+﻿using DOTNET_WPF_Shop.Modules.Auth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOTNET_WPF_Shop.Modules.Start
+{
+    public class StartProvider
+    {
+        public void RedirectToSignupPage(Start view)
+        {
+            view.Hide();
+            new Signup().ShowDialog();
+            view.Show();
+        }
+
+        public void RedirectToSigninPage(Start view)
+        {
+            view.Hide();
+            new Signin().ShowDialog();
+            view.Show();
+        }
+    }
+}
