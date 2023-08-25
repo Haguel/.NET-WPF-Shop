509e25596cb8c1ade99ee5c478f92e5fbd42f4d2
GlebHal <hammanov.gleb@gmail.com>
2023-08-25 14:25:15 +0300

    Small additions to start view

---
+++ DOTNET-WPF-Shop/Modules/Start/ViewConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOTNET_WPF_Shop.Modules.Start
+{
+    public class ViewConfig
+    {
+        public int buttonWidth { get; } = 200;
+        public int buttonHeight { get; } = 40;
+    }
+}
