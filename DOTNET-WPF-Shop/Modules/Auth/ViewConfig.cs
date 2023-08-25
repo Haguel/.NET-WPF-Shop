003ced03306fa343596ecf5ad91c0bcc974e8b75
GlebHal <hammanov.gleb@gmail.com>
2023-08-25 14:24:59 +0300

    Made auth views

---
+++ DOTNET-WPF-Shop/Modules/Auth/ViewConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOTNET_WPF_Shop.Modules.Auth
+{
+    public class ViewConfig
+    {
+        public int textBoxWidth { get; } = 300;
+        public int textBoxHeight { get; } = 40;
+    }
+}
