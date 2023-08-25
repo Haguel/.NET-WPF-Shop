30aac4786f8201d0be616b814ff7730db810253d
GlebHal <hammanov.gleb@gmail.com>
2023-08-25 13:14:52 +0300

    Added ViewConfig

---
+++ DOTNET-WPF-Shop/Modules/ViewConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOTNET_WPF_Shop.Modules
+{
+    public class ViewConfig
+    {
+        public int bigFontSize { get; } = 20;
+        public int defaultFontSize { get; } = 16;
+        public int smallFontSize { get; } = 14;
+    }
+}
