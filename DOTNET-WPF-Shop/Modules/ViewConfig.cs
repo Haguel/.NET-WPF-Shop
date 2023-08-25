ecda9b835d1915d9f501c223de644a7c7ef01986
GlebHal <hammanov.gleb@gmail.com>
2023-08-25 14:24:59 +0300

    Made auth views

--- DOTNET-WPF-Shop/Modules/ViewConfig.cs
+++ DOTNET-WPF-Shop/Modules/ViewConfig.cs
@@ -11,5 +11,7 @@ namespace DOTNET_WPF_Shop.Modules
         public int bigFontSize { get; } = 20;
         public int defaultFontSize { get; } = 16;
         public int smallFontSize { get; } = 14;
+        public string mainColor { get; } = "#DE493F";
+        public string acceptColor { get; } = "#299F37";
     }
 }
