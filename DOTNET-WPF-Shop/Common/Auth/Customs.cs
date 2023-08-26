d6cb2babbcf892aa4d3554002a46bd0ed69e3549
GlebHal <hammanov.gleb@gmail.com>
2023-08-26 13:01:09 +0300

    Made full rework of prop and elements system

---
+++ DOTNET-WPF-Shop/Common/Auth/Customs.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace DOTNET_WPF_Shop.Common.Auth
+{
+    public class AuthTextBox : TextBox
+    {
+        public AuthTextBox()
+        {
+            Properties properties = new Properties();
+            Utils utils = new Utils();
+
+            utils.SetCenterAlignment(this);
+            utils.SetFontSize(this, Utils.FontSizes.Default);
+            utils.SetSizes(this, properties.textBoxWidth, properties.textBoxHeight);
+        }
+    }
+
+    public class AuthTitle : TextBlock
+    {
+        public AuthTitle()
+        {
+            Utils utils = new Utils();
+
+            utils.SetCenterAlignment(this);
+            utils.SetFontSize(this, Utils.FontSizes.Big);
+        }
+    }
+}
---
+++ DOTNET-WPF-Shop/Utils.cs
@@ -0,0 +1,52 @@
+﻿using DOTNET_WPF_Shop.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace DOTNET_WPF_Shop
+{
+    class Utils
+    {
+        private GlobalProperties globalProperties = new GlobalProperties();
+        public enum FontSizes
+        {
+            Small,
+            Default,
+            Big
+        }
+
+        public void SetCenterAlignment(FrameworkElement element)
+        {
+            element.HorizontalAlignment = HorizontalAlignment.Center;
+            element.VerticalAlignment = VerticalAlignment.Center;
+        }
+
+        public void SetFontSize(FrameworkElement element, FontSizes fontSizes) 
+        {
+            int currentFontSize = globalProperties.defaultFontSize;
+
+            if (fontSizes == FontSizes.Small) currentFontSize = globalProperties.smallFontSize;
+            else if (fontSizes == FontSizes.Default) currentFontSize = globalProperties.defaultFontSize;
+            else if (fontSizes == FontSizes.Big) currentFontSize = globalProperties.bigFontSize;
+
+            if(element is Control control) control.FontSize = currentFontSize;
+            if(element is TextBlock textBlock) textBlock.FontSize = currentFontSize;
+        }
+
+        public void SetSizes(FrameworkElement element, int width, int height)
+        {
+            element.Width = width;
+            element.Height = height;
+        }
+
+        public SolidColorBrush getSolidColorBrushFromHex(string hex)
+        {
+            return new SolidColorBrush((Color)ColorConverter.ConvertFromString(hex));
+        }
+    }
+}
