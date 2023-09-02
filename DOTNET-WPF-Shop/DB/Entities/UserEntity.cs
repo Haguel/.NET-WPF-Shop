608821c0e857d6975a58e6bba4f5aa2f46fc24bd
GlebHal <hammanov.gleb@gmail.com>
2023-09-02 13:49:55 +0300

    Added db and user Entity

---
+++ DOTNET-WPF-Shop/DB/Entities/UserEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOTNET_WPF_Shop.DB.Entities
+{
+    public class UserEntity
+    {
+        public Guid Id { get; set; }
+        public String Username { get; set; }
+        public String Email { get; set; }
+        public String PasswordHash { get; set; }
+    }
+}
