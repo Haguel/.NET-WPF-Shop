75bea7747c295cdcf02f944aa2d5cedc723a9ea3
GlebHal <hammanov.gleb@gmail.com>
2023-09-02 12:17:15 +0300

    Auth WIP

--- DOTNET-WPF-Shop/Modules/Auth/Dto/SigninUserDto.cs
+++ DOTNET-WPF-Shop/Modules/Auth/Dto/SigninUserDto.cs
@@ -1,14 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace DOTNET_WPF_Shop.Modules.Auth.Dto
 {
     class SigninUserDto
     {
-        public string email { get; }
-        public string password { get; }
+        [Required(ErrorMessage = "Email is required")]
+        [Display(Name = "email")]
+        [EmailAddress(ErrorMessage = "Invalid email")]
+        public string email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [Display(Name = "password")]
+        [MinLength(6, ErrorMessage = "Password minimum length is 6 characters")]
+        public string password { get; set; }
     }
 }
