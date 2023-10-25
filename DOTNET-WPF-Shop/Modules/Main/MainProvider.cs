e26852ed49053cffad30162b313b441a5f8f45a4
GlebHal <hammanov.gleb@gmail.com>
2023-10-25 11:31:28 +0300

    Bug fixes + adjustments

--- DOTNET-WPF-Shop/Modules/Main/MainProvider.cs
+++ DOTNET-WPF-Shop/Modules/Main/MainProvider.cs
@@ -1,10 +1,5 @@
-﻿using DOTNET_WPF_Shop.DB;
-using DOTNET_WPF_Shop.DB.Entities;
-using DOTNET_WPF_Shop.Modules.Cart;
+﻿using DOTNET_WPF_Shop.DB.Entities;
 using DOTNET_WPF_Shop.Modules.Product;
-using DOTNET_WPF_Shop.Modules.User;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -20,13 +15,6 @@ namespace DOTNET_WPF_Shop.Modules.Main
             List<ProductEntity> products = selectedProducts == null ? new() : new(selectedProducts);
 
             return products;
-        }
-
-        public void RedirectToCartPage(Main view, Cart.Cart cartView)
-        {
-            view.Hide();
-            cartView.ShowDialog();
-            view.Show();
         }
     }
 }
